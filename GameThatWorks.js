76553d0f046471d2f8a7504360b29c04e9cabc55
22116wl <22116wl@hvhs.school.nz>
2025-02-24 20:26:29 +0000

    high score

--- GameThatWorks.js
+++ GameThatWorks.js
@@ -1,10 +1,11 @@
 const movementSpeed = 10;
-const wallThickness = 3;
+const coinTimer = 7;
 var backgroundColour;
 
 var scene;
 
 var score = 0;
+var highScore = score;
 
 function setup() {
     cnv = new Canvas(windowWidth, windowHeight);
@@ -12,12 +13,6 @@ function setup() {
     player.color = 'cyan';
 
     coins = new Group()
-
-    roof = 		new Sprite(windowWidth/2,   windowHeight,	windowWidth,	wallThickness,	"s");
-	floor = 	new Sprite(windowWidth/2, 	0,  			windowWidth,	wallThickness,	"s");
-	rightWall = new Sprite(windowWidth, 	windowHeight/2,	wallThickness,	windowWidth, 	"s");
-	leftWall = 	new Sprite(0,				windowHeight/2,	wallThickness,	windowWidth,	"s");
-
     scene = 'menu';
 
     function deleteCoin(collider1, collider2) {
@@ -29,6 +24,13 @@ function setup() {
 	player.collides(coins, deleteCoin);
 }
 
+function startGame() {
+    scene = 'game';
+    score = 0;
+    coinDelay = 100;
+    newCoin();
+}
+
 function draw() {
     if (scene == 'game') { gameScreen() }
     if (scene == 'menu') { menuScreen() }
@@ -38,9 +40,11 @@ function draw() {
 function newCoin() {
     coin = new Sprite(random(0, windowWidth), random(0, windowHeight), 40);
     coin.color = 'yellow';
+    coin.spawnTime = millis();
+
     coins.add(coin);
-    coins.timeRemaining = 5*60;
 }
+
 
 function menuScreen() {
     allSprites.visible = false;
@@ -54,32 +58,35 @@ function menuScreen() {
     textSize(40);
     text("Press 'Space' to begin!", windowWidth/2, windowHeight/2 + 20);
 
-    if (kb.pressing('space')) {
-        scene = 'game';
-        score = 0;
-        timer = 20;
-        newCoin();
-    }
+    textSize(20);
+    textAlign('left');
+    text("High Score: " + highScore, 10, 30);
+
+    if (kb.pressing('space')) { startGame(); }
 }
 
 function finishedScreen() {
     background('cyan');
     allSprites.visible = false;
 
-    textSize(35);
+    textSize(45);
     fill("#FFFFFF");
     textAlign('center');
-    text("Game Finished!", windowWidth/2, windowHeight/2 - 50);
-    text("Final Score: " + score, windowWidth/2, windowHeight/2 - 10);
+    text("Game Over!", windowWidth/2, windowHeight/2 - 75);
+    textSize(25);
+    text("Final Score: " + score, windowWidth/2, windowHeight/2 - 15);
+    textSize(22.5);
+    text("High Score: " + highScore, windowWidth/2, windowHeight/2 + 15);
 
-    text("Press 'Esc' to return to menu", windowWidth/2, windowHeight/2 + 60);
+    textSize(20);
+    text("Press 'Esc' to return to menu", windowWidth/2, windowHeight/2 + 70);
+    text("Press 'Space' to replay", windowWidth/2, windowHeight/2 + 95);
 
-    if (kb.pressing('escape')) {
-        scene = 'menu';
-    }
+    if (kb.pressing('escape')) { scene = 'menu'; }
+    if (kb.pressing('space')) { startGame(); }
 }
 
-var timer;
+var coinDelay;
 
 function gameScreen() {
     background('#FFFFFF');
@@ -90,37 +97,45 @@ function gameScreen() {
     textSize(30);
     text('Score: ' + score, 20, 40);
 
-    textAlign('right');
-    text('Time Remaining: ' + timer, windowWidth-20, 40);
+    console.log(coinDelay);
 
-    if (frameCount % 120 == 0) {
-        counter = 0;
+    if (frameCount % coinDelay == 0) {
         newCoin();
     }
 
-    for (var i = 0; i < coins.length; i++) {
-        cache
+    if (frameCount % 40 == 0 && coinDelay > 40) {
+        coinDelay --;
+    }
 
-        if (coins[i].spawnTime + 50 < millis()) {
-            coins[i].remove()  
+    for (var i = 0; i < coins.length; i++) {
+        coin = coins[i];
+
+        if (coin.spawnTime + coinTimer*1000 < millis()) {
+            scene = 'finished';
+            coins.removeAll();
+            
+            if (score > highScore) {
+                highScore = score;
+            }
+
+            continue;
         }
+
+        fill(230, 230, 230);
+        rect(coin.x - 20, coin.y + 35, 40, 13);
+
+        fill(0, 255, 0);
+        rect(coin.x - 20, coin.y + 35, (1 - (millis() - coin.spawnTime)/(coinTimer*1000)) * 40, 13);
     }
     
-    if (frameCount % 60 == 0) {
-        timer --;
-    }
-
-    if (timer <= 0) {
-        scene = 'finished';
-        coins.removeAll();
-    }
 
     // Player movement
-    if (kb.pressing('W')) { player.vel.y = -movementSpeed; }
-    else if (kb.pressing('S')) { player.vel.y = movementSpeed; }
+    if (kb.pressing('W') && player.y > 50 ) { player.vel.y = -movementSpeed; }
+    else if (kb.pressing('S') && player.y < windowHeight - 50 ) { player.vel.y = movementSpeed; }
     else { player.vel.y = 0 }
 
-    if (kb.pressing('A')) { player.vel.x = -movementSpeed; }
-    else if (kb.pressing('D')) { player.vel.x = movementSpeed; }
+    if (kb.pressing('A') && player.x > 50 ) { player.vel.x = -movementSpeed; }
+    else if (kb.pressing('D') && player.x < windowWidth - 50 ) { player.vel.x = movementSpeed; }
     else { player.vel.x = 0 }
+
 }
